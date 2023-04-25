d572600086e04c4632124e95fde39105d01defe0
Alexandra Hendriks <kolozsvari.alexandra25@gmail.com>
2023-04-25 15:01:59 +0200

    first-commit

--- main.py
+++ main.py
@@ -2,4 +2,16 @@
 __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
 
+'''You need to master the following to complete this assignment:
+    Creating and indexing dictionaries
+    Writing functions and function arguments
+    Using various operators.'''
+
 # Add your code after this line
+
+def greet(name, template="Hello, <name>!"):
+    print(f"{template} {name}!")
+    return f"{template}, {name}!"
+
+
+greet("Sandy", "Whats up <name>")
