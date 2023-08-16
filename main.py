fe8b94cef7588567f43e59afb79306c18c14d127
alexandrahendriks <kolozsvari.alexandra25@gmail.com>
2023-08-16 13:41:24 +0200

    small fixes

--- main.py
+++ main.py
@@ -2,22 +2,13 @@
 __winc_id__ = '7b9401ad7f544be2a23321292dd61cb6'
 __human_name__ = 'arguments'
 
-'''You need to master the following to complete this assignment:
-    Creating and indexing dictionaries
-    Writing functions and function arguments
-    Using various operators.'''
-
-# Add your code after this line
-
 # Greet Template
 def greet(name, template='Hello, <name>!'):
-    print(template.replace("<name>", name))
     return(template.replace("<name>", name))
 
-print(greet("Sandy", "What's up <name>?"))
+print(greet("Lucas", "What's up <name>?"))
 
 # Force
-
 def force(mass, body="earth"):
     planets = {
         'sun': 274,
@@ -32,17 +23,16 @@ def force(mass, body="earth"):
         'moon': 1.6,
         'pluto': 0.6,
     }
-    if body in planets.keys():
+    if (body in planets):
         force = mass * planets[body]
         return force
     
-force(2, "pluto")
+print(force(2, "pluto"))
 
 # Gravity
-
 def pull(m1, m2, d):
     G = 6.674*(10**-11)
     gravitaion_pull = G * ((m1*m2)/(d**2))
     return gravitaion_pull
 
-pull(800, 1500, 3)
+print(pull(800, 1500, 3))
